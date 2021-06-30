85b12ee356f96a9c9d3a13744f68b35809b5ad8f
Hafize Aksoy <hafize.teacher@gmail.com>
2021-06-30 11:26:31 -0700

    new branch1

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -9,5 +9,6 @@ public class Practice {
         System.out.println("commit1 github");
         System.out.println("commit2 github");
         System.out.println("new branch");
+        System.out.println("new branch1");
     }
 }
